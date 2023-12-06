689dbd0e557cd599b6a78961472bd94f6df0217b
Manel <maneldelahozrodriguez@gmail.com>
2023-12-06 13:16:26 +0100

    first commit

--- exercise_01/transformStringToCollection.test.js
+++ exercise_01/transformStringToCollection.test.js
@@ -15,4 +15,52 @@ describe("Given function transformStringToArray", () => {
       expect(result).toEqual([]);
     });
   });
+  describe("When the function is called with a value of type array", () => {
+    test("Then an empty array should be returned.", () => {
+      // Arrange
+      const value = [8, 9, 10, 11, 12, 13];
+
+      // Act
+      const result = transformStringToArray(value, onError, onSuccess);
+
+      // Assert
+      expect(result).toEqual([]);
+    });
+  });
+  describe("When the function is called with a value of type boolean", () => {
+    test("Then an empty array should be returned.", () => {
+      // Arrange
+      const value = true;
+
+      // Act
+      const result = transformStringToArray(value, onError, onSuccess);
+
+      // Assert
+      expect(result).toEqual([]);
+    });
+  });
+  describe("When the function is called with an empty string value", () => {
+    test("Then an empty array should be returned.", () => {
+      // Arrange
+      const value = "";
+
+      // Act
+      const result = transformStringToArray(value, onError, onSuccess);
+
+      // Assert
+      expect(result).toEqual([]);
+    });
+  });
+  describe("When the function is called with a value of type string", () => {
+    test("Then an empty array should be returned.", () => {
+      // Arrange
+      const value = "Hello, my name is Manel";
+
+      // Act
+      const result = transformStringToArray(value, onError, onSuccess);
+
+      // Assert
+      expect(result).toEqual(["Hello,", "my", "name", "is", "Manel"]);
+    });
+  });
 });
